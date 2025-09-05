abae0aa3fa5deb9fc90d5c602f63f025ff282c15
Alejoreal1 <ahiguita696@gmail.com>
2025-09-05 10:36:05 -0500

    Agrego rutas

--- src/app/registrar/registrar.ts
+++ src/app/registrar/registrar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Nav } from "../shared/components/nav/nav";
 import { Footer } from "../shared/components/footer/footer";
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-registrar',
-  imports: [Nav, Footer],
+  imports: [Nav, Footer,RouterModule],
   templateUrl: './registrar.html',
   styleUrl: './registrar.css'
 })
